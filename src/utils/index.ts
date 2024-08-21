882eaaf23a899b579ec8696e9d931eaa5bd3dcf3
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-21 18:05:37 +0300

    feat: create helper for 2d arrays

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export function map2D<T, R>(
+  twoDArray: T[][],
+  callback: (item: T, index: [number, number]) => R
+): R[] {
+  const newArr: R[] = []
+  twoDArray.forEach((row, y) =>
+    row.forEach((item, x) => newArr.push(callback(item, [x, y])))
+  )
+  return newArr
+}

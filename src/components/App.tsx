d355656adff6bf426cd28b54a20b636bfade0e5d
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-21 18:06:09 +0300

    feat: use several comps in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,46 @@
-import { useGridHeight, useGridWidth } from '@/store'
+import { useGrid, useGridHeight, useGridWidth } from '@/store'
 import Svg from '@/components/Svg'
+import Background from '@/components/Background'
+import Tiles from '@/components/Tiles'
+import Tile from '@/components/Tile'
+import { map2D } from '@/utils'
 
 const TILE = {
   WIDTH: 10,
   HEIGHT: 10
 }
+const SVG_WIDTH_MULTIPLIER = 4
 
 function App() {
   const gridWidth = useGridWidth()
   const gridHeight = useGridHeight()
+  const grid = useGrid()
+
+  const viewBoxWidth = gridWidth * TILE.WIDTH
+  const viewBoxHeight = gridHeight * TILE.HEIGHT
+  const outerWidth = SVG_WIDTH_MULTIPLIER * viewBoxWidth
+
+  const tileUseId = 'tileProto'
 
   return (
     <Svg
-      gridWidth={gridWidth}
-      gridHeight={gridHeight}
-      tileWidth={TILE.WIDTH}
-      tileHeight={TILE.HEIGHT}
+      viewBoxWidth={viewBoxWidth}
+      viewBoxHeight={viewBoxHeight}
+      outerWidth={outerWidth}
     >
-      <></>
+      <Background viewBoxWidth={viewBoxWidth} viewBoxHeight={viewBoxHeight} />
+      <Tiles useId={tileUseId} tileWidth={TILE.WIDTH} tileHeight={TILE.HEIGHT}>
+        {map2D(grid, (terrainSymbol, [x, y]) => (
+          <Tile
+            useId={tileUseId}
+            tileWidth={TILE.WIDTH}
+            tileHeight={TILE.HEIGHT}
+            terrainSymbol={terrainSymbol}
+            x={x}
+            y={y}
+          ></Tile>
+        ))}
+      </Tiles>
     </Svg>
   )
 }

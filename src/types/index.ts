d5a7001e64bce9d12e4c48d375cac667d8855edb
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-22 14:22:09 +0300

    feat: create types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,31 @@
+export type Store = {
+  grid: TerrainSymbol[][]
+  setGrid: () => void
+}
+
+export type Entity = {
+  id: string
+}
+
 export type TerrainSymbol = '.' | 'w' | 'f'
+
+export type Owner = 'ai' | 'player'
+
+export type Position = [number, number]
+
+/** Character info that comes from json */
+export type CharacterData = {
+  name: string
+  position: Position
+  owner: Owner
+} & Entity
+
+export type Movable = {
+  position: Position
+  path: Array<PathSegment>
+}
+
+export type PathSegment = {
+  pathCost: number
+  position: Position
+}

87814201e70addd1f47dc96859f2d86b98b88fe2
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-22 14:07:08 +0300

    refactor: use immutable params

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 export function map2D<T, R>(
   twoDArray: T[][],
-  callback: (item: T, index: [number, number]) => R
+  callback: (item: T, x: number, y: number) => R
 ): R[] {
   const newArr: R[] = []
   twoDArray.forEach((row, y) =>
-    row.forEach((item, x) => newArr.push(callback(item, [x, y])))
+    row.forEach((item, x) => newArr.push(callback(item, x, y)))
   )
   return newArr
 }

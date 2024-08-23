0e5773d75aae39da0f05f82cfd64bc2d5e50c539
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-23 17:01:54 +0300

    refactor: use Scenario in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,15 @@
-import { useCharactersList, useGrid } from '@/store'
-import Svg from '@/components/Svg'
-import Background from '@/components/Background'
-import Tiles from '@/components/Tiles'
-import Tile from '@/components/Tile'
-import GridLines from '@/components/GridLines'
-import CharacterTiles from '@/components/CharacterTiles'
-import CharacterTile from '@/components/CharacterTile'
-import { map2D } from '@/utils'
-import { TILE_CSS } from '@/config'
+import Scenario from '@/components/Scenario'
+import scenario1json from '@/data/scenario1.json'
+import { ScenarioDataScema } from '@/types'
 
-const tileCssSize: [number, number] = [TILE_CSS.WIDTH, TILE_CSS.HEIGHT]
+const scenario1Data = ScenarioDataScema.safeParse(scenario1json)
+const { success: isSuccess } = scenario1Data
 
 function App() {
-  const grid = useGrid()
-  const characters = useCharactersList()
-
-  return (
-    <Svg tileCssSize={tileCssSize}>
-      <Background />
-      <Tiles>
-        {map2D(grid, (terrainSymbol, x, y) => (
-          <Tile
-            key={`${x}-${y}`}
-            terrainSymbol={terrainSymbol}
-            x={x}
-            y={y}
-          ></Tile>
-        ))}
-      </Tiles>
-      <GridLines />
-      <CharacterTiles>
-        {characters.map((char) => (
-          <CharacterTile
-            key={char.id}
-            x={char.position[0]}
-            y={char.position[1]}
-            owner={char.owner}
-          />
-        ))}
-      </CharacterTiles>
-    </Svg>
-  )
+  if (isSuccess) {
+    return <Scenario scenarioData={scenario1Data.data} />
+  }
+  return <div>Failed to validate scenario</div>
 }
 
 export default App

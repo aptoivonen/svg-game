1e67f65cc1076c7812bd5929a395497963c53ca5
Aleksi Toivonen <aptoivonen@gmail.com>
2024-08-30 16:13:09 +0300

    feat: add name to character box

--- src/components/CharacterInfoBox.tsx
+++ src/components/CharacterInfoBox.tsx
@@ -7,6 +7,7 @@ type CharacterInfoBoxProps = {
 function CharacterInfoBox({ character }: CharacterInfoBoxProps) {
   return (
     <div className="absolute bottom-1 right-1 z-10 h-[250px] w-[150px] bg-[rgba(0,0,0,0.7)] p-4 text-white">
+      <p className="text-lg">{character.name}</p>
       <p>Id: {character.id}</p>
       <p>Pos: {`(${character.position[0]}, ${character.position[1]})`}</p>
       <p>Owner: {character.owner}</p>

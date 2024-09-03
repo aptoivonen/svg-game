1b15e4449522c6ab880225a69139ddd1974a10b2
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-03 16:40:40 +0300

    change: make new api for path

--- src/utils/path/path.ts
+++ src/utils/path/path.ts
@@ -1,42 +1,89 @@
+import { TERRAIN } from '@/config'
 import Graph from './Graph'
 import search from './search'
-import type { PathSegment, Position } from '@/types'
+import type { Path, Position, TerrainSymbol, Positioned } from '@/types'
+import { isEqual } from '@/utils'
 
 /**
  * Finds path to end position. If no path, returns path to position closest to end.
  * @param {Object} params
- * @param {number[][]} params.map
- * @param {Array<Position>} params.characterPositions
- * @param {Position} params.start
- * @param {Position} params.end
- * @returns {Array<{pathCost: number, position: Position}>}
+ * @param {Position} params.targetPosition
+ * @param {Positioned} params.characterToMove
+ * @param {TerrainSymbol[][]} params.grid
+ * @param {Positioned[]} params.charactersList
+ * @returns
  */
 export default function path({
-  map,
-  characterPositions,
-  start,
-  end
+  targetPosition,
+  characterToMove,
+  grid,
+  charactersList
 }: {
-  map: number[][]
-  characterPositions: Position[]
-  start: Position
-  end: Position
-}): PathSegment[] {
-  const graph = new Graph(map)
-  const [startX, startY] = start
-  const [endX, endY] = end
-  // flip x and y, algorithm has them the other way around
-  const startNode = graph.grid[startY][startX]
-  // flip x and y, algorithm has them the other way around
-  const endNode = graph.grid[endY][endX]
-  characterPositions.forEach(([charX, charY]) => {
-    // weight 0 => cost is infinite
-    graph.grid[charY][charX].weight = 0
-  })
+  targetPosition: Position
+  characterToMove: Positioned
+  grid: TerrainSymbol[][]
+  charactersList: Positioned[]
+}): Path {
+  const [startX, startY] = characterToMove.position
+  const [endX, endY] = targetPosition
 
-  return search(graph, startNode, endNode, { closest: true }).map((node) => ({
-    pathCost: node.pathCost,
-    // flip x and y, algorithm has them the other way around
-    position: [node.y, node.x]
-  }))
+  const movementCostGrid: number[][] = getMovementCostGrid(grid)
+
+  mutateSetCharacterPositions(movementCostGrid, charactersList, targetPosition)
+
+  const graph = new Graph(movementCostGrid)
+
+  // flip x and y, Graph has them the other way around
+  const startNode = graph.grid[startY][startX]
+  const endNode = graph.grid[endY][endX]
+
+  const resultPath = search(graph, startNode, endNode, { closest: true }).map(
+    (node) => ({
+      pathCost: node.pathCost,
+      position: [node.y, node.x] as [number, number]
+    })
+  )
+
+  mutateSetDropLastSegmentIfOccupied(resultPath, targetPosition, charactersList)
+
+  return resultPath
+}
+
+/** set movement costs. 0 means not passable */
+function getMovementCostGrid(terrainGrid: TerrainSymbol[][]): number[][] {
+  return terrainGrid.map((row) =>
+    row.map((terrainSymbol) => TERRAIN[terrainSymbol].movementCost)
+  )
+}
+
+/** set character positions. 0 means position taken */
+function mutateSetCharacterPositions(
+  movementCostGrid: number[][],
+  charactersList: Positioned[],
+  targetPosition: Position
+): void {
+  charactersList.forEach((character) => {
+    // if a character is standing on target, ignore him to find correct path
+    if (isEqual(character.position, targetPosition)) return
+    const [charX, charY] = character.position
+    movementCostGrid[charY][charX] = 0
+  })
+}
+
+/** drop last path segment if path was calculated ignoring the occupying character.
+ * Algorithm fings suboptimal path if character was noted first.
+ */
+function mutateSetDropLastSegmentIfOccupied(
+  path: Path,
+  targetPosition: Position,
+  charactersList: Positioned[]
+): void {
+  const lastPosition = path.at(-1)
+  if (lastPosition) {
+    charactersList.forEach((character) => {
+      if (isEqual(character.position, targetPosition)) {
+        path.pop()
+      }
+    })
+  }
 }

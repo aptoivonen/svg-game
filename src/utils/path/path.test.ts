e4efc8d87da9a854421fb84affaec6d39113ae06
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-03 16:41:04 +0300

    test: write and change tests for path

--- src/utils/path/path.test.ts
+++ src/utils/path/path.test.ts
@@ -1,25 +1,48 @@
 import path from './path'
 
 test('path finds route with a blocking character included', () => {
-  const pathWithObstacles = path({
-    map: [
-      [1, 1, 1, 1],
-      [0, 0, 1, 1],
-      [1, 0, 1, 1],
-      [1, 1, 1, 1],
-      [1, 1, 1, 1]
+  const calculatedPath = path({
+    targetPosition: [0, 4],
+    characterToMove: { position: [0, 0] },
+    grid: [
+      ['.', '.', '.', '.'],
+      ['w', 'w', '.', '.'],
+      ['.', 'w', '.', '.'],
+      ['.', '.', '.', '.'],
+      ['.', '.', '.', '.']
     ],
-    characterPositions: [[2, 1]],
-    start: [0, 0],
-    end: [0, 4]
+    charactersList: [{ position: [2, 1] }]
   })
 
-  expect(pathWithObstacles.map((segment) => segment.position)).toEqual([
+  expect(calculatedPath.map((segment) => segment.position)).toEqual([
     [1, 0],
     [2, 0],
     [3, 1],
     [2, 2],
     [1, 3],
     [0, 4]
+  ])
+})
+
+test('path finds route next to another character', () => {
+  const calculatedPath = path({
+    targetPosition: [0, 4],
+    characterToMove: { position: [0, 0] },
+    grid: [
+      ['.', '.', '.', '.'],
+      ['w', 'w', '.', '.'],
+      ['.', 'w', '.', '.'],
+      ['.', '.', '.', '.'],
+      ['.', '.', '.', '.']
+    ],
+    charactersList: [{ position: [2, 1] }, { position: [0, 4] }]
+  })
+
+  expect(calculatedPath.map((segment) => segment.position)).toEqual([
+    [1, 0],
+    [2, 0],
+    [3, 1],
+    [2, 2],
+    [1, 3]
   ])
 })

56ca7191ed9b890bf64c09f539ad40a4d14c8b70
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-18 12:43:12 +0300

    feat: format nice validation error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,24 @@
 import ScenarioPage from '@/features/ScenarioPage'
 import scenario1json from '@/data/scenario1.json'
 import { ScenarioDataSchema } from '@/types'
+import { fromError } from 'zod-validation-error'
 
 const scenario1Data = ScenarioDataSchema.safeParse(scenario1json)
-const { success: isSuccess } = scenario1Data
+const { success: isSuccess, error } = scenario1Data
+const { message: errorMessage } = fromError(error)
 
 function App() {
   if (isSuccess) {
     return <ScenarioPage scenarioData={scenario1Data.data} />
   }
-  return <div>Failed to validate scenario</div>
+  return (
+    <div className="grid h-full place-items-center text-xl text-white">
+      <div className="max-w-xl">
+        <p>Failed to validate scenario:</p>
+        <p>{errorMessage}</p>
+      </div>
+    </div>
+  )
 }
 
 export default App

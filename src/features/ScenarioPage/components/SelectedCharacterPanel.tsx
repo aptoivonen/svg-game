5de965d0b9f38c90285fa3b9e15b432616f8315c
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-18 11:57:54 +0300

    feat: make selected character panel smaller

--- src/features/ScenarioPage/components/SelectedCharacterPanel.tsx
+++ src/features/ScenarioPage/components/SelectedCharacterPanel.tsx
@@ -6,7 +6,7 @@ type SelectedCharacterPanelProps = {
 
 function SelectedCharacterPanel({ character }: SelectedCharacterPanelProps) {
   return (
-    <div className="absolute bottom-1 left-1/2 z-10 h-[250px] w-[600px] -translate-x-1/2 bg-[rgba(0,0,0,0.7)] p-4 text-white">
+    <div className="absolute bottom-0 left-1/2 z-10 h-[200px] w-[600px] -translate-x-1/2 bg-[rgba(0,0,0,0.7)] p-4 text-white">
       <p>{character.name}</p>
     </div>
   )

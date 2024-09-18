e049552ea2931a82dce02d5b7c1cdae59c5d2bbb
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-18 11:56:32 +0300

    feat: move infobox to top

--- src/features/ScenarioPage/components/CharacterInfoBox.tsx
+++ src/features/ScenarioPage/components/CharacterInfoBox.tsx
@@ -6,7 +6,7 @@ type CharacterInfoBoxProps = {
 
 function CharacterInfoBox({ character }: CharacterInfoBoxProps) {
   return (
-    <div className="absolute bottom-1 right-1 z-10 h-[250px] w-[150px] bg-[rgba(0,0,0,0.7)] p-4 text-white">
+    <div className="absolute right-1 top-1 z-10 h-[250px] w-[150px] bg-[rgba(0,0,0,0.7)] p-4 text-white">
       <p className="text-lg">{character.name}</p>
       <p>Id: {character.id}</p>
       <p>Pos: {`(${character.position[0]}, ${character.position[1]})`}</p>

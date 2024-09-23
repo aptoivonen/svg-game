0912a8023aede3215b42619356cc0bd4e4c570ee
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-23 10:44:29 +0300

    feat: change a terrain name

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,21 @@
 export const TERRAIN = {
-  '.': { movementCost: 100, name: 'Plain' },
+  '.': { movementCost: 100, name: 'Grass' },
   w: { movementCost: 0, name: 'Water' },
   f: { movementCost: 200, name: 'Forest' }
 } as const
 
-/** Terrain tile size in outer svg viewbox context */
-export const TILE = {
-  WIDTH: 10,
-  HEIGHT: 10
+export const TILE_DATA = {
+  '.': { id: 'grass', indexX: 17, indexY: 1 },
+  f: { id: 'forest', indexX: 31, indexY: 12 },
+  w: { id: 'water', indexX: 7, indexY: 1 }
+}
+
+/** Terrain tile size in original tile set */
+export const TILE_IMAGE_SIZE = 20
+
+export const TILESET_IMAGE_DIMENSIONS = {
+  WIDTH: 640,
+  HEIGHT: 780
 }
 
 /** Terrain tile visual size in css pixels */

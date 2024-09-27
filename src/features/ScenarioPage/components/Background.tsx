b83df5fd4dca8152de979017baf48f5d99a5769f
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-27 15:50:07 +0300

    feat: make background black

--- src/features/ScenarioPage/components/Background.tsx
+++ src/features/ScenarioPage/components/Background.tsx
@@ -3,7 +3,7 @@ import { useViewboxSize } from './Svg'
 function Background() {
   const [viewBoxWidth, viewBoxHeight] = useViewboxSize()
 
-  return <rect width={viewBoxWidth} height={viewBoxHeight} fill="#ccc"></rect>
+  return <rect width={viewBoxWidth} height={viewBoxHeight} fill="black"></rect>
 }
 
 export default Background

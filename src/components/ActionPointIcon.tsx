571593ac444fefd5894b2a5c15de7f0002e2c956
Aleksi Toivonen <aptoivonen@gmail.com>
2024-09-30 15:59:50 +0300

    style: remove prop detail

--- src/components/ActionPointIcon.tsx
+++ src/components/ActionPointIcon.tsx
@@ -1,6 +1,6 @@
 import type { ComponentPropsWithoutRef } from 'react'
 
-type ActionPointIconProps = unknown & ComponentPropsWithoutRef<'svg'>
+type ActionPointIconProps = ComponentPropsWithoutRef<'svg'>
 export default function ActionPointIcon(props: ActionPointIconProps) {
   return (
     <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
